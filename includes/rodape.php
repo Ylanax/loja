91869a299a6ea90bb91397bf9f13652c4984184f
Ylana Pereira <ylana.pereira2004@outlook.com>
2025-06-02 22:26:43 -0300

    aula segunda

---
+++ includes/rodape.php
@@ -0,0 +1,3 @@
+<footer class="rodape">
+        <p class="texto-rodape"><?php echo date("Y"); ?>&copy; Desenvolvido por <strong>[Ylana Pereira]</strong> | Projeto fictício sem fins comerciais.</p>
+    </footer>
